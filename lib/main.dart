9400b112f3712d5353cd9deb894e41acc611aae7
unknown <you@example.com>
2021-05-12 13:19:03 +0300

    another commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Meow'),
     );
   }
 }
